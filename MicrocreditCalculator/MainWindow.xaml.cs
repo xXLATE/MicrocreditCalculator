0b4ef222f1f677cd9be81e53debb389c216a57a6
xXLATE <bruno-roberto@mail.ru>
2021-06-25 17:56:07 +0300

    add limits

--- MicrocreditCalculator/MainWindow.xaml.cs
+++ MicrocreditCalculator/MainWindow.xaml.cs
@@ -45,6 +45,18 @@ namespace MicrocreditCalculator
             catch
             {
                 MessageBox.Show("Введите сумму займа и кол-во дней");
+                return;
+            }
+
+            if (SummaZaima.Text == "")
+            {
+                MessageBox.Show("Введите сумму займа");
+                return;
+            }
+            if (Convert.ToDouble(SummaZaima.Text) > 500000)
+            {
+                MessageBox.Show("Сумма займа не может быть больше 500.000");
+                return;
             }
 
             for (int i = 0; i < LoanTerm; i++)
@@ -66,29 +78,59 @@ namespace MicrocreditCalculator
             int counter = 0;
             foreach (TextBox box1 in daysStack.Children)
             {
-                bets[counter] = Convert.ToDouble(box1.Text);
-                counter++;
+                if (box1.Text == "")
+                {
+                    MessageBox.Show("Заполните все поля");
+                    return;
+                }
+                if (Convert.ToDouble(box1.Text) > 1)
+                {
+                    MessageBox.Show("Принимаются значения не более 1%\nПример: 0,9");
+                    return;
+                }
+                else
+                {
+                    bets[counter] = Convert.ToDouble(box1.Text);
+                    counter++;
+                }
             }
 
-            persents[0] = Convert.ToInt32(bets[0] * 100);
+            try
+            {
+                persents[0] = Convert.ToInt32(bets[0] * 100);
+            }
+            catch
+            {
+                MessageBox.Show("Введите сумму займа и кол-во дней");
+                return;
+            }
+
+            
             persentsStack.Children.Add(new TextBox { Text = Convert.ToString(persents[0]) });
 
             for (int i = 1; i < LoanTerm; i++)
             {
                 persents[i] = Convert.ToInt32((bets[i] * 100) + persents[i - 1]);
-                persentsStack.Children.Add(new TextBox { Text = Convert.ToString(persents[i]) });
+                persentsStack.Children.Add(new TextBox { Text = Convert.ToString(persents[i]), IsReadOnly = true });
             }
 
             for (int i = 0; i < LoanTerm; i++)
             {
                 sums[i] = persents[i] + Convert.ToInt32(SummaZaima.Text);
-                sumStack.Children.Add(new TextBox { Text = Convert.ToString(sums[i]) });
+                sumStack.Children.Add(new TextBox { Text = Convert.ToString(sums[i]), IsReadOnly = true });
             }
 
             SummaViplati.Text = Convert.ToString(sums[LoanTerm - 1]);
             SummaProcentov.Text = Convert.ToString(persents[LoanTerm - 1]);
             Stavka.Text = Convert.ToString(Convert.ToDouble(persents[LoanTerm - 1]) / Convert.ToDouble(SummaZaima.Text) / Convert.ToDouble(LoanTerm) * 100) + "%";
 
+            double maxSizePayment = (Convert.ToDouble(SummaZaima.Text) * 1.5) + Convert.ToDouble(SummaZaima.Text);
+
+            if (Convert.ToDouble(SummaZaima.Text) > maxSizePayment)
+            {
+                MessageBox.Show("Размер выплаты превышает лимит");
+                return;
+            }
 
 
             //UIElement element = daysStack.Children[0];
